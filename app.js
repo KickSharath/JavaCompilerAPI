6117fcf13252cab0cc0b38791a7db58c7a38376b
KICK Sharath <kicksharath234@gmail.com>
2023-09-27 14:17:56 +0530

    Logs changes and start

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const fs = require('fs').promises;
 const app = express();
 const PORT = 3000;
 
+
+const logs = [];
+
 app.use(bodyParser.text());
 
 app.post('/compile', async (req, res) => {
@@ -19,6 +22,7 @@ app.post('/compile', async (req, res) => {
         if (compileError) {
             console.error(`Compilation Error: ${compileError}`);
             res.status(500).send(`Compilation Error: ${compileError}`);
+            logs.push(`Compilation Error: ${compileError}`);
             return;
         }
 
@@ -26,15 +30,22 @@ app.post('/compile', async (req, res) => {
         if (executionError) {
             console.error(`Execution Error: ${executionError}`);
             res.status(500).send(`Execution Error: ${executionError}`);
+            logs.push(`Execution Error: ${executionError}`);
             return;
         }
 
         console.log(`Output: ${stdout}`);
         res.send(`Output: ${stdout}`);
+        logs.push(`Output: ${stdout}`);
     } catch (error) {
         console.error(`Server Error: ${error.message}`);
         res.status(500).send(`Server Error: ${error.message}`);
+        logs.push(`Server Error: ${error.message}`);
     }
+});
+
+app.get('/logs', (req, res) => {
+    res.json(logs);
 });
 
 app.listen(PORT, () => {
